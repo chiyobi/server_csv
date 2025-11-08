1caf9da4cbc164941f4fd77a5d66c9f8415e7665
chiyobi <155046495+chiyobi@users.noreply.github.com>
2025-11-08 09:15:26 -0800

    test

--- dist/db.js
+++ dist/db.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.groups = exports.userIdToGroupIds = exports.groupIdToUserIds = exports.friends = exports.friendRequests = exports.emailsToId = exports.users = exports.auth = exports.tempTokens = exports.families = void 0;
+exports.deleteACarpool = exports.savedTrips = exports.userIdToCarpoolId = exports.carpoolIdToUserId = exports.carpools = exports.groups = exports.userIdToGroupIds = exports.groupIdToUserIds = exports.friends = exports.friendRequests = exports.emailsToId = exports.users = exports.auth = exports.tempTokens = exports.families = void 0;
 exports.families = new Map();
 exports.tempTokens = new Map();
 exports.auth = new Map();
@@ -11,3 +11,26 @@ exports.friends = new Map();
 exports.groupIdToUserIds = new Map();
 exports.userIdToGroupIds = new Map();
 exports.groups = new Map();
+exports.carpools = new Map();
+exports.carpoolIdToUserId = new Map();
+exports.userIdToCarpoolId = new Map();
+exports.savedTrips = new Map();
+const deleteACarpool = (userId, carpoolId) => {
+    const allUserIdsOfCarpool = exports.carpoolIdToUserId.get(carpoolId);
+    if (allUserIdsOfCarpool) {
+        for (const uId of allUserIdsOfCarpool) {
+            const uCarpools = exports.carpools.get(uId);
+            if (uCarpools) {
+                exports.carpools.set(uId, uCarpools.filter((c) => c.id !== carpoolId));
+            }
+            if (exports.userIdToCarpoolId.has(uId)) {
+                const carpoolIds = exports.userIdToCarpoolId.get(uId);
+                if (carpoolIds) {
+                    exports.userIdToCarpoolId.set(userId, carpoolIds.filter((cId) => cId !== carpoolId));
+                }
+            }
+        }
+        exports.carpoolIdToUserId.delete(carpoolId);
+    }
+};
+exports.deleteACarpool = deleteACarpool;
