dd8c17844c8d7b09ed4e114745dfad2b64f7749a
chiyobi <155046495+chiyobi@users.noreply.github.com>
2025-11-08 09:22:00 -0800

    test

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -90,7 +90,7 @@ userRouter.post("/new", async (req, res, next) => {
         res.json({ success: true });
     }
     catch (error) {
-        // res.status(500).json({ error });
+        res.status(500).json({ error });
     }
 });
 // userRouter.put(
