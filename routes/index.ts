26411758da5c70a46c5fd3d816ecece5d26bb98c
chiyobi <155046495+chiyobi@users.noreply.github.com>
2025-11-07 23:21:13 -0800

    test

--- routes/index.ts
+++ routes/index.ts
@@ -10,7 +10,7 @@ router.get("/", (req: Request, res: Response) => {
   res.json({ message: "🚀 TypeScript Node Server is running!" });
 });
 
-// router.use("/api/user", userRouter);
+router.use("/api/user", userRouter);
 // router.use("/api/network", networkRouter);
 // router.use("/api/family", familyRouter);
 // router.use("/api/carpool", carpoolRouter);
