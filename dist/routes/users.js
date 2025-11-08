bcaf7effa70172b9c5e343d2215fd625d32d49ff
chiyobi <155046495+chiyobi@users.noreply.github.com>
2025-11-08 09:25:06 -0800

    test

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -2,6 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const db_1 = require("../db");
+// import { friends, friendRequests, FriendProfile } from "./network";
+// import { families } from "./family";
+// import {
+//   carpoolIdToUserId,
+//   userIdToCarpoolId,
+//   carpools,
+//   deleteACarpool,
+//   Carpool,
+// } from "./carpool";
+const utils_1 = require("../utils");
 const userRouter = (0, express_1.Router)();
 userRouter.post("/signin", async (req, res, next) => {
     const { email, password } = req.body;
@@ -55,7 +65,7 @@ userRouter.get("/verify", async (req, res, next) => {
 });
 userRouter.post("/new", async (req, res, next) => {
     const { email, password } = req.body;
-    // const id = generateUUID();
+    const id = (0, utils_1.generateUUID)();
     try {
         // if (emailsToId.has(email)) {
         //   throw "Email already exists. Use a different email.";
